6b4e37d6b2f4e309157613514cf022fa7d3c2b6b
RashmiSingh287 <rashmi.25b01010668@abes.ac.in>
2026-04-23 14:44:44 +0530

    js

---
+++ bootstrap/app.js
@@ -0,0 +1,13 @@
+
+// var name="Rashmi";
+// var age=30;
+// console.log(name);
+
+// var age=50;
+// console.log(age);
+var name="Rashmi";
+var age=30;
+console.log(name);
+
+var age=50;
+console.log(age);
