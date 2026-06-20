3861a4ad0083af91dcca324475080ae54ac00f58
RashmiSingh287 <rashmi.25b01010668@abes.ac.in>
2026-06-20 22:39:08 +0530

    UPDATE JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,48 +1,48 @@
-console.log("apna clg")
-console.log("i am learning javascript")
-/*variables*/
-name="tonystark";
-console.log(name);
-age=24;
-console.log(age);
-x=null;
-console.log(x);
-y = undefined;
-console.log(y);
+// console.log("apna clg")
+// console.log("i am learning javascript")
+// /*variables*/
+// name="tonystark";
+// console.log(name);
+// age=24;
+// console.log(age);
+// x=null;
+// console.log(x);
+// y = undefined;
+// console.log(y);
 
-isfollow=true;
+// isfollow=true;
 
 
-let fullName="Rashmi";
-fullname="shreya";
+// let fullName="Rashmi";
+// fullname="shreya";
 
-{let a=9;}
-// object collection of key  value pair
-const student={
-    name:"rahul",
-    class:10,
-    cgpa:8
-};
-student["cgpa"]=student["cgpa"]+1;
-console.log(student.cgpa);
+// {let a=9;}
+// // object collection of key  value pair
+// const student={
+//     name:"rahul",
+//     class:10,
+//     cgpa:8
+// };
+// student["cgpa"]=student["cgpa"]+1;
+// console.log(student.cgpa);
 
-const product={
-    name:"Ballpen",
-    offer:5,
-    price:270,
-    isDeal:true,
-    rating:5
-};
-console.log(product);
+// const product={
+//     name:"Ballpen",
+//     offer:5,
+//     price:270,
+//     isDeal:true,
+//     rating:5
+// };
+// console.log(product);
 
-const profile={
-    name:"sharradha khapra",
-    followers:2345,
-    followings:278,
-    isFollow:true
-};
-console.log(profile);
-console.log(typeof profile["name"]);
+// const profile={
+//     name:"sharradha khapra",
+//     followers:2345,
+//     followings:278,
+//     isFollow:true
+// };
+// console.log(profile);
+// console.log(typeof profile["name"]);
 
 /*************lec 2 operators  */
 // single line comment 
@@ -50,17 +50,58 @@ console.log(typeof profile["name"]);
 operator*/
 
 // arithmetic operator 
- let a=5;
- let b=4;
- console.log("a+b=",a+b);
-  console.log("a-b=",a-b);
-   console.log("a*b=",a*b);
-  console.log("a/b=",a/b);
- console.log("a%b=",a%b);
-  console.log("a**b=",a**b);
-//unary operator ,,increment++
-let c=5;
-let d=8;
-console.log("c=",c);
+//  let a=5;
+//  let b=4;
+//  console.log("a+b=",a+b);
+//   console.log("a-b=",a-b);
+//    console.log("a*b=",a*b);
+//   console.log("a/b=",a/b);
+//  console.log("a%b=",a%b);
+//   console.log("a**b=",a**b);
+// //unary operator ,,increment++
+// let c=5;
+// let d=8;
+// console.log("c=",c);
 
-console.log(++c);
+// console.log(++c);
+
+
+/*ASSIGNMENT OPERATORS**********************/
+// let a=5;
+// let b=9;
+// a+=4;
+// a/=4;
+// a**=4;
+// a%=4;
+// console.log("a=",a)
+
+/****comparison operator */
+/*
+equal to == , equal to& type ===,
+not equal != */
+
+// let d=4;
+// let c=2;
+// console.log("c!=d",c!=d);
+
+/*LOGICAL OPERATOR*******************/
+/*
+logical and && 
+logical or ||
+not !
+*/
+
+//CONDITIONAL STATEMENt
+//  let age=25;
+// if(age>=25){
+//     console.log("can vote");
+// }
+let mode="dark";
+let color;
+if(mode=="dark"){
+    color="black";
+}
+else{
+    color="white";
+}
+console.log(color);
