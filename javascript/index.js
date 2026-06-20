c21677c87879c9a2099c3455189a51dfec9efc04
RashmiSingh287 <rashmi.25b01010668@abes.ac.in>
2026-06-20 21:18:57 +0530

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,46 @@
+console.log("apna clg")
+console.log("i am learning javascript")
+/*variables*/
+name="tonystark";
+console.log(name);
+age=24;
+console.log(age);
+x=null;
+console.log(x);
+y = undefined;
+console.log(y);
+
+isfollow=true;
+
+
+let fullName="Rashmi";
+fullname="shreya";
+
+{let a=9;}
+// object collection of key  value pair
+const student={
+    name:"rahul",
+    class:10,
+    cgpa:8
+};
+student["cgpa"]=student["cgpa"]+1;
+console.log(student.cgpa);
+
+const product={
+    name:"Ballpen",
+    offer:5,
+    price:270,
+    isDeal:true,
+    rating:5
+};
+console.log(product);
+
+const profile={
+    name:"sharradha khapra",
+    followers:2345,
+    followings:278,
+    isFollow:true
+};
+console.log(profile);
+console.log(typeof profile["name"]);
+
