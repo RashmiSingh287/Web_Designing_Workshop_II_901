380b594d169bb5339de24358083eccc32bac785d
RashmiSingh287 <rashmi.25b01010668@abes.ac.in>
2026-06-23 15:28:05 +0530

    practical

---
+++ end sem practical/script.js
@@ -0,0 +1,10 @@
+function findSum() {
+    let n = Number(document.getElementById("num").value);
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    document.getElementById("result").innerHTML = "Sum = " + sum;
+}
